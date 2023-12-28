dc0f60c5d76c05e0eb2644589ab2d4744706a6ac
Vishal Jawadwar <vsjawadwar@gmail.com>
2023-12-28 11:56:20 +0530

    Password generator porject has been completed by using useRef useCallback useEffect

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,32 +1,84 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [length,setLength]=useState(8);
-  const [numberAllowed,setNumberAllowed]=useState(false);
-  const [characterAllowed,setCharAllowed]=useState(false);
-  const [password,setPassword]=useState("");
-  
-  const passwordGenerator = useCallback(()=>{
-    let pass="";
-    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if(numberAllowed) str+= "0123456789";
-    if(characterAllowed) str+="!@#$%^&*()_{}[]~`''";
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const passRef=useRef(null);
 
-    for(let i=0; i<=length; i++){
-      let char= Math.floor(Math.random()*length + 1);
-      pass+=str.charAt[char];
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*()_{}[]~`''";
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
     setPassword(pass);
-  },[length,numberAllowed,characterAllowed,setPassword]);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+
+  const copyToClipboard=useCallback(()=>{
+    window.navigator.clipboard.writeText(password);
+    passRef.current?.select();
+  },[password]);
+
+  useEffect(()=>{
+    passwordGenerator();
+  },[length,numberAllowed,characterAllowed,passwordGenerator]);
+
 
   return (
-    <>
-     <div style={{display:'flex',justifyContent:'center', marginTop:200}}>
-     <input style={{height:40,width:300,marginRight:20}} type="text" value={password} placeholder='password' readOnly/>
-     <button type="button" class="btn btn-primary">Copy Password</button>
-     </div>
-    </>
+    <div className='container'>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 200 }}>
+        <input style={{ height: 40, width: 300, marginRight: 20 }} 
+        type="text" 
+        value={password} 
+        placeholder='password' 
+        readOnly 
+        ref={passRef}
+        />
+        <button 
+          type="button" 
+          className="btn btn-primary"
+          onClick={copyToClipboard}>Copy Password</button>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 10 }}>
+        <input 
+          type="range" 
+          min={6} 
+          max={100} 
+          value={length} 
+          onChange={(e) => { setLength(e.target.value) }} 
+        />
+        <label>Length: {length}</label>
+        
+        <input 
+          className='mx-1'
+          type="checkbox"   
+          defaultChecked={numberAllowed} 
+          id='numberInput' 
+          onChange={()=>{
+          setNumberAllowed((prev)=> !prev);
+          }} 
+          />
+          <label className='mx-1'>Numbers</label>
+
+          <input 
+          className='mx-1'
+          type="checkbox"   
+          defaultChecked={characterAllowed} 
+          id='characterInput' 
+          onChange={()=>{
+          setCharAllowed((prev)=> !prev);
+          }} 
+          />
+          <label className='mx-1'>Special Characters</label>
+      </div>
+    </div>
   )
 }
 
