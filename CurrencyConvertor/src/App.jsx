5a48f0e8ac69f63d504399710bdb111cbf09ee02
Vishal Jawadwar <vsjawadwar@gmail.com>
2024-01-02 23:32:57 +0530

    Currency Convertor Mini Project has been initiated

---
+++ CurrencyConvertor/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+     <h1 className='text-3xl bg-orange-500'>Jay Shree Ram</h1>
+     
+     
+    </>
+  )
+}
+
+export default App
