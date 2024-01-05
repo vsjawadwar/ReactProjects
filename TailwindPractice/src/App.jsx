7810c5acb3c355d30c2ec944dfe21e91637ee8ab
Vishal Jawadwar <vsjawadwar@gmail.com>
2024-01-05 21:03:52 +0530

    Tailwind CSS basic card created by using utility classes

--- TailwindPractice/src/App.jsx
+++ TailwindPractice/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Practice1 from './components/Practice1'
+import Practice2Card from './components/Practice2Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Practice1 />
+      {/* <Practice1 /> */}
+      <Practice2Card />
     </>
   )
 }
---
+++ TailwindPractice/src/components/Practice2Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Practice2Card() {
+  return (
+    <div className='grid place-content-center h-screen'>
+      <div className='p-6 max-w-sm mx-auto bg-white rounded-lg border-2 border-black shadow-lg flex items-center space-x-4 '>
+        <div>
+            <img className='h-20 w-20' src="https://images.pexels.com/photos/15241238/pexels-photo-15241238/free-photo-of-asphalt-road-through-desert.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Not Loaded" />
+        </div>
+        <div className='text-lg'>
+            Tailwind CSS
+            <p className=' text-slate-500'>By Vishal Jawadwar</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Practice2Card
