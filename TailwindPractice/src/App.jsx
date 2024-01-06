cf627245687dcd9bac588d03db59472e8c57859b
Vishal Jawadwar <vsjawadwar@gmail.com>
2024-01-06 23:39:34 +0530

    Tailwind CSS Card 2

--- TailwindPractice/src/App.jsx
+++ TailwindPractice/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Practice1 from './components/Practice1'
 import Practice2Card from './components/Practice2Card'
+import Practice3Card from './components/Practice3Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,8 @@ function App() {
   return (
     <>
       {/* <Practice1 /> */}
-      <Practice2Card />
+      {/* <Practice2Card /> */}
+      <Practice3Card />
     </>
   )
 }
---
+++ TailwindPractice/src/components/Practice3Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Practice3Card() {
+  return (
+    <div>
+      <h1>Jay Shree Ram</h1>
+    </div>
+  )
+}
+
+export default Practice3Card
