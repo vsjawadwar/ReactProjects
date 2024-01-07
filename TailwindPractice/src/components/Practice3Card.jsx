beaf5440881c47030e4d4e4c901f89e4dd9b6232
Vishal Jawadwar <vsjawadwar@gmail.com>
2024-01-07 21:51:17 +0530

    Tailwind CSS Practice card 3 [Responsive]

--- TailwindPractice/src/components/Practice3Card.jsx
+++ TailwindPractice/src/components/Practice3Card.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 function Practice3Card() {
   return (
-    <div>
-      <h1>Jay Shree Ram</h1>
+    <div className='grid place-content-center'>
+      <div className='max-w-sm mx-auto bg-white rounded-xl border-2 border-gray-700 md:max-w-2xl '>
+        <div className='md:flex'>
+        <div>
+            <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://images.pexels.com/photos/18296591/pexels-photo-18296591/free-photo-of-sarangpur-hanuman-ji.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        </div>
+        <div className='p-8'>
+            <div className='uppercase tracking-wide text-sm text-orange-500 font-semibold'>
+            <h1>Jay Shree Ram</h1>
+            </div>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
